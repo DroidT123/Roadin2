325b01ea37bc02730268eb40140ee451fd2ac4a8
DroidT <36623625+DroidT123@users.noreply.github.com>
2018-02-27 14:46:59 +0530

    signin

--- app/src/main/java/com/example/roadin/MainActivity.java
+++ app/src/main/java/com/example/roadin/MainActivity.java
@@ -95,6 +95,9 @@ public class MainActivity extends AppCompatActivity
             fragmentManager.beginTransaction().replace(R.id.mainLayout,adFragment).commit();
 
         } else if (id == R.id.nav_slideshow) {
+            SignInFragment signInFragment = new SignInFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.mainLayout,signInFragment).commit();
 
         } else if (id == R.id.nav_manage) {
 
--- app/src/main/java/com/example/roadin/SignInFragment.java
+++ app/src/main/java/com/example/roadin/SignInFragment.java
@@ -1,12 +1,24 @@
 package com.example.roadin;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -20,6 +32,9 @@ public class SignInFragment extends Fragment {
     }
 
 
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,6 +46,44 @@ public class SignInFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
 
+        // Create and launch sign-in intent
+        mAuth = FirebaseAuth.getInstance();
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(this.getActivity(), MainActivity.class));
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(this.getActivity(), MainActivity.class));
+
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                Toast.makeText(this.getActivity(), "Not Success", Toast.LENGTH_LONG).show();
+                // ...
+            }
+        }
     }
 }
