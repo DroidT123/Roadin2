b6b77662b5f7a9b5237705ac6c3bfd180bc269b4
DroidT <36623625+DroidT123@users.noreply.github.com>
2018-03-14 14:18:51 +0530

    camera_frag

---
+++ app/src/main/java/com/example/roadin/CameraFragment.java
@@ -0,0 +1,81 @@
+package com.example.roadin;
+
+import android.*;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static android.content.Context.LOCATION_SERVICE;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CameraFragment extends Fragment   {
+
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+    //View view;
+    View view;
+    Button firstButton;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+
+       /* @Override
+        public void onViewCreated (View view, @Nullable Bundle savedInstanceState){
+            super.onViewCreated(view, savedInstanceState);*/
+
+        firstButton = (Button) view.findViewById(R.id.firstButton);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), camActivity.class);
+                getActivity().startActivity(intent);
+
+            }
+        });
+        return view;
+    }
+
+}
